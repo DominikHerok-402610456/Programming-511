beac60d305e07954e4d69393d2d4b14a5fe25044
DominikHerok-402610456 <402610456@my.richfield.ac.za>
2026-09-09 15:52:46 +0200

    Added File Handler file with 2x functions for basic handling

---
+++ file_handler.py
@@ -0,0 +1,17 @@
+# ==========================================
+# FILEHANDLER.PY
+# ==========================================
+
+#Function to append to my files
+def save_record(file_name,line):
+    with open(file_name,'a') as f:
+            f.write(line + '\n')
+
+#Function to read lines and return list of lines from txt file.
+def read_lines(filename):
+    try:
+        with open(filename, 'r') as f:
+            return f.readlines()
+
+    except FileNotFoundError:
+        return []
